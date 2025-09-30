cd2dde2f0b8f128d06b943c756d762da059ba782
ArturMota19 <artur19rodriguesmota@gmail.com>
2025-09-30 09:13:51 -0300

    FEAT: Adiciona imagem lena em escala cinza com open cv

---
+++ ed1/main.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+import time
+
+def main():
+    # imagem lena, escala cinza
+    img = cv2.imread("lena.pgm", cv2.IMREAD_GRAYSCALE)
+
+    if img is None:
+        print("imagem nao encontrada na root")
+        return
+
+
+
+if __name__ == "__main__":
+    main()
